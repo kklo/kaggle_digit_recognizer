2033735649afaf61c42909206aceaad8454be54b
KK <lokwunkit@gmail.com>
2013-09-08 15:46:21 +0800

    fixed the bug in zoning function. Now multiple parameters can be passed to the operation

--- src/features/zoning.py
+++ src/features/zoning.py
@@ -6,12 +6,12 @@
 # Starting from width w, w/2, w/4 to the min_width provided.
 # The function operator will be called with the subimage as
 # the parameter.
-def apply_to_zones(image, image_width, min_width, operator):
+def apply_to_zones(image, image_width, min_width, operator, *args):
     w = image_width
     while(w >= min_width):
         half_w = w/2
         for i in xrange(0, image_width, w):
             for j in xrange(0, image_width, w):
                 subimage = image[i:i+w, j:j+w]
-                operator(subimage)
+                operator(subimage, *args)
         w = half_w
